5b8c399be8f7a7fb2727eb87e9bd3ced6af09a9e
vietanhcao <vietanhcao1994@gmail.com>
2023-08-30 15:28:22 +0700

    feat(router): init router

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import useRouteElement from './useRouteElement'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const routerElement = useRouteElement()
 
-  return (
-    <>
-      <div className=''>
-        <a href='https://react.dev' target='_blank' rel='noreferrer'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className='card'>
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className='read-the-docs'>Click on the Vite and React logos to learn more</p>
-    </>
-  )
+  return <div>{routerElement}</div>
 }
 
 export default App
