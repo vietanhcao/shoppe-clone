3b56e552c364a0b1fcd37ce093f3567cee4111c8
vietanhcao <vietanhcao1994@gmail.com>
2023-09-11 14:46:32 +0700

    add test

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
-import { defineConfig } from 'vite'
+// eslint-disable-next-line import/no-unresolved
+import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react-swc'
 import path from 'path'
 import { visualizer } from 'rollup-plugin-visualizer'
@@ -6,6 +7,9 @@ import { visualizer } from 'rollup-plugin-visualizer'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), visualizer()],
+  test: {
+    environment: 'jsdom'
+  },
   server: {
     port: 1994
   },
